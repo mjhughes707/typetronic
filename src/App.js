9e55b52eaad7ef3b47e7d2a1bab3f23a46108402
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-14 15:17:32 -0800

    converted App into controlled form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
   return (
     <div>
       <h1>TypeTronic</h1>
-      <textarea />
+      <textarea onChange={handleChange} value={text} />
       <h4>The amount of time remaining: </h4>
       <button>START</button>
       <h1>Word Count: </h1>
