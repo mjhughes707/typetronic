672d0716239efa82816e97e3d2f1b926b4160469
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-14 14:50:09 -0800

    built out basic App template, linked Google Font, imported CSS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>TypeTronic</h1>
+      <textarea />
+      <h4>The amount of time remaining: </h4>
+      <button>START</button>
+      <h1>Word Count: </h1>
+    </div>
+  );
+}
+
+export default App;
