0afe8e61d1b990442f565fe616829a517aaaf517
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-14 19:36:49 -0800

    added start and end game functions + updated useEffect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const START_TIME = 5;
+
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(START_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   function handleChange(e) {
     const { value } = e.target;
     setText(value);
   }
 
-  function countWords(text) {
+  function startGame() {
+    setText("");
+    setWordCount(0);
+    setTimeRemaining(START_TIME);
+    setIsTimeRunning(true);
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(getWordCount(text));
+  }
+
+  function getWordCount(text) {
     const wordsArr = text.trim().split(" ");
     const filteredWords = wordsArr.filter(word => word !== "");
     return filteredWords.length;
   }
 
   useEffect(() => {
-    if (timeRemaining > 0) {
+    if (isTimeRunning && timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining(prevTime => prevTime - 1);
       }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
     }
-  }, [timeRemaining]);
+  }, [timeRemaining, isTimeRunning]);
 
   return (
     <div>
       <h1>TypeTronic</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>The amount of time remaining: {timeRemaining}</h4>
-      <button
-        onClick={() => {
-          countWords(text);
-        }}
-      >
-        START
-      </button>
-      <h1>Word Count: </h1>
+      <button onClick={startGame}>START</button>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
