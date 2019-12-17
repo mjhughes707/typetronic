ddbab0d0a2cd5e8f9ea2679ff24d818a79c7a429
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-17 15:11:35 -0800

    added a custom hook to help separate concerns

--- src/App.js
+++ src/App.js
@@ -1,63 +1,31 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+import useWordGame from "./hooks/useWordGame";
 
 function App() {
-  const START_TIME = 5;
-
-  const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(START_TIME);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-  const textareaRef = useRef(null);
-
-  function handleChange(e) {
-    const { value } = e.target;
-    setText(value);
-  }
-
-  function startGame() {
-    setText("");
-    setWordCount(0);
-    setTimeRemaining(START_TIME);
-    setIsTimeRunning(true);
-    textareaRef.current.disabled = false;
-    textareaRef.current.focus();
-  }
-
-  function endGame() {
-    setIsTimeRunning(false);
-    setWordCount(getWordCount(text));
-  }
-
-  function getWordCount(text) {
-    const wordsArr = text.trim().split(" ");
-    const filteredWords = wordsArr.filter(word => word !== "");
-    return filteredWords.length;
-  }
-
-  useEffect(() => {
-    if (isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining(prevTime => prevTime - 1);
-      }, 1000);
-    } else if (timeRemaining === 0) {
-      endGame();
-    }
-  }, [timeRemaining, isTimeRunning]);
+  const {
+    textBoxRef,
+    handleChange,
+    text,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+    wordCount,
+  } = useWordGame();
 
   return (
     <div>
-      <h1>TypeTronic</h1>
+      <h1>How fast do you type?</h1>
       <textarea
-        ref={textareaRef}
+        ref={textBoxRef}
         onChange={handleChange}
-        disabled={!isTimeRunning}
         value={text}
+        disabled={!isTimeRunning}
       />
-      <h4>The amount of time remaining: {timeRemaining}</h4>
+      <h4>Time remaining: {timeRemaining}</h4>
       <button onClick={startGame} disabled={isTimeRunning}>
-        START
+        Start
       </button>
-      <h1>Word Count: {wordCount}</h1>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 }
