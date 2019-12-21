9abe94db7ee8ee17ae1b134204fd7e3d5d12122e
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-21 11:36:11 -0800

    Updates

---
+++ precache-manifest.14e32805194e7b276bf63afdae9f26c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ed14d1dc3a4f1d552cc0fa52ec00f7",
+    "url": "/typetronic/index.html"
+  },
+  {
+    "revision": "9eba842fb49d06e59738",
+    "url": "/typetronic/static/css/main.4709c8b7.chunk.css"
+  },
+  {
+    "revision": "e84ff5d2312fc7a1efb9",
+    "url": "/typetronic/static/js/2.8121dd19.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/typetronic/static/js/2.8121dd19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9eba842fb49d06e59738",
+    "url": "/typetronic/static/js/main.d04ed666.chunk.js"
+  },
+  {
+    "revision": "c886391df3c7afb3ac60",
+    "url": "/typetronic/static/js/runtime-main.42f0c838.js"
+  }
+]);
